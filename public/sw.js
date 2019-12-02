d4ae8db9e0485971dead2885830fc7f191ea7023
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-12-02 12:13:47 +0300

    update webpack config

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,54 @@
+const CACHE_NAME = `CINEMADDICT_V1.0`;
+
 self.addEventListener(`install`, evt => {
-  const openCache = caches.open(`STATIC_V1.0`).then(cache => {
-    return cache.addAll([`/`, `/index.html`, `/bundle.js`]);
-  });
-  evt.waitUntil(openCache);
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/images/emoji/angry.png`,
+        `/images/emoji/puke.png`,
+        `/images/emoji/sleeping.png`,
+        `/images/emoji/smile.png`,
+        `/images/emoji/trophy.png`,
+        `/images/icons/icon-favorite-active.svg`,
+        `/images/icons/icon-favorite.svg`,
+        `/images/icons/icon-watched-active.svg`,
+        `/images/icons/icon-watched.svg`,
+        `/images/icons/icon-watchlist-active.svg`,
+        `/images/icons/icon-watchlist.svg`,
+        `/images/posters/made-for-each-other.png`,
+        `/images/posters/popeye-meets-sinbad.png`,
+        `/images/posters/sagebrush-trail.jpg`,
+        `/images/posters/santa-claus-conquers-the-martians.jpg`,
+        `/images/posters/the-dance-of-life.jpg`,
+        `/images/posters/the-great-flamarion.jpg`,
+        `/images/posters/the-man-with-the-golden-arm.jpg`,
+        `/images/background.png`,
+        `/images/bitmap.png`,
+        `/images/bitmap@2x.png`,
+        `/images/bitmap@3x.png`,
+        `/css/main.css`,
+        `/css/normalize.css`,
+        `/js/bundle.js`,
+        `/js/bundle.js.map`
+      ]);
+    })
+  );
 });
+
 self.addEventListener(`activate`, () => {
   console.log(`sw activated`);
+});
+self.addEventListener(`fetch`, evt => {
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then(response => {
+        return response ? response : fetch(evt.request);
+      })
+      .catch(err => {
+        throw err;
+      })
+  );
 });
