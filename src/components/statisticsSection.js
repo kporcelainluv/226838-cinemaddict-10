4aff4da3ee36f7fddc2ade14ad14f8461a0f0cfa
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-01-15 18:10:35 +0300

    update formatting

--- src/components/statisticsSection.js
+++ src/components/statisticsSection.js
@@ -1,4 +1,4 @@
-import { AbstractComponent } from "./abstractComponent";
+import {AbstractComponent} from "./abstractComponent";
 
 export class StatisticsSection extends AbstractComponent {
   constructor() {
--- src/controllers/stats-chart-controller.js
+++ src/controllers/stats-chart-controller.js
@@ -1,6 +1,6 @@
-import { StatsChart } from "../components/statsChart";
-import { render, unrender, getGenresByKeysVals } from "../utils";
-import { Position } from "../consts";
+import {StatsChart} from "../components/statsChart";
+import {render, unrender, getGenresByKeysVals} from "../utils";
+import {Position} from "../consts";
 
 export class StatsChartController {
   constructor(container) {
@@ -13,9 +13,9 @@ export class StatsChartController {
     this._chart = new StatsChart();
     this._chart.createChart(getGenresByKeysVals(films));
     render(
-      this._container.getElement(),
-      this._chart.getElement(),
-      Position.BEFOREEND
+        this._container.getElement(),
+        this._chart.getElement(),
+        Position.BEFOREEND
     );
   }
   unrender() {
