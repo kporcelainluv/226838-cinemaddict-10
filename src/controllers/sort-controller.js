56928ec61f22baf9ff3b25abd4c57cca4bb64b1d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-01-16 14:02:59 +0300

    wip

--- src/controllers/sort-controller.js
+++ src/controllers/sort-controller.js
@@ -1,6 +1,6 @@
-import {render} from "../utils";
-import {Sort} from "../components/sort";
-import {Position} from "../consts";
+import { render } from "../utils";
+import { Sort } from "../components/sort";
+import { Position } from "../consts";
 
 export class SortController {
   constructor(container, onSortChange) {
@@ -11,7 +11,7 @@ export class SortController {
 
   init() {
     render(this._container, this._sort.getElement(), Position.AFTERBEGIN);
-    this._sort.onSortBtnClick((evt) => {
+    this._sort.onSortBtnClick(evt => {
       evt.preventDefault();
       const type = evt.target.dataset.sortType;
       this._sort.makeButtonStateActive(type);
