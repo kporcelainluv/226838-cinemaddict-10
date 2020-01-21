07fefd27abc9333a983fbdef9153041f157e8537
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-01-21 18:55:42 +0300

    update storage

--- src/store.js
+++ src/store.js
@@ -5,54 +5,21 @@ export const Store = class {
   }
 
   setItem({ key, item }) {
-    const items = this.getAll();
-    items[key] = item;
-    this._storage.setItem(this._storeKey, JSON.stringify(items));
-  }
-
-  removeComment({ key }) {
-    const films = this.getAll();
-    const comments = films.slice(0).map(film => film.comments);
-    const newComments = comments.map(comment => {
-      if (comment.includes(key)) {
-        return comment.filter(commentId => commentId !== key);
-      }
-    });
-    const newFilms = films.map(
-      (film, index) => (film.comments = newComments[index])
-    );
-
-    this._storage.setItem(this._storeKey, JSON.stringify(newFilms));
-  }
-  getItem({ key }) {
-    const items = this.getAll();
-    return items[key];
-  }
-
-  addComments({ key, comments }) {
-    const films = this.getAll();
-    const newFilms = films.map(
-      (film, index) => (film.comments = comments[index])
-    );
-    this._storage.setItem(this._storeKey, JSON.stringify(newFilms));
+    const store = this.getAll();
+    const newValue = {};
+    Object.assign(newValue, store, { [key]: item });
+    this._storage.setItem(this._storeKey, JSON.stringify(newValue));
   }
 
   getAll() {
-    const emptyItems = {};
-    const items = this._storage.getItem(this._storeKey);
-
-    if (!items) {
-      return emptyItems;
-    }
-
     try {
-      return JSON.parse(items);
-    } catch (e) {
-      console.error(`Error parse items. Error: ${e}. Items: ${items}`);
-      return emptyItems;
+      const store = this._storage.getItem(this._storeKey);
+      if (store) {
+        return JSON.parse(store);
+      }
+      return {};
+    } catch (error) {
+      return {};
     }
-  }
-  updateAllFilms({ key, films }) {
-    this._storage.setItem(this._storeKey, JSON.stringify(films));
   }
 };
